52ea67e2add8060cf831bb8d7d76bd3a07c0d98b
frankuxui <fisdray@gmail.com>
2024-09-06 13:16:51 +0200

    site-config

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -2,5 +2,9 @@ import { defineConfig } from '@rsbuild/core'
 import { pluginReact } from '@rsbuild/plugin-react'
 
 export default defineConfig({
-  plugins: [ pluginReact() ]
+  plugins: [ pluginReact() ],
+  server: {
+    publicDir: 'public',
+    port: 3000
+  }
 })
